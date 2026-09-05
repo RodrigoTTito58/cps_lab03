6a2cb82774d36c801558e54abd12a5ef35667925
RodrigoTTito58 <rodrigottito32@gmail.com>
2026-09-04 20:33:05 -0500

    se agrego el StudentController al controllers

--- src/main/java/pe/edu/tecsup/lab03/controllers/StudentController.java
+++ src/main/java/pe/edu/tecsup/lab03/controllers/StudentController.java
@@ -1,24 +1,4 @@
 package pe.edu.tecsup.lab03.controllers;
 
-import pe.edu.tecsup.lab03.entities.StudentEntity;
-import pe.edu.tecsup.lab03.services.StudentService;
-
-import java.util.List;
-
 public class StudentController {
-
-    private StudentService studentService = new StudentService();
-
-    public void agregarEstudiante(Long id, String nombre, String apellido, String correo) {
-        StudentEntity student = new StudentEntity(id, nombre, apellido, correo);
-        studentService.registrarEstudiante(student);
-        System.out.println("Estudiante agregado: " + nombre + " " + apellido);
-    }
-
-    public void mostrarEstudiantes() {
-        List<StudentEntity> lista = studentService.listarEstudiantes();
-        for (StudentEntity s : lista) {
-            System.out.println("ID: " + s.getId() + " - " + s.getNombre() + " " + s.getApellido());
-        }
-    }
 }
