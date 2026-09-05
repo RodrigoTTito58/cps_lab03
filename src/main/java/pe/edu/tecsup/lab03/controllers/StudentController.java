9ecba46df3da12f27712c94feedf82c6df7b252d
RodrigoTTito58 <rodrigottito32@gmail.com>
2026-09-04 20:57:33 -0500

    Cambios sprint-2

--- src/main/java/pe/edu/tecsup/lab03/controllers/StudentController.java
+++ src/main/java/pe/edu/tecsup/lab03/controllers/StudentController.java
@@ -1,4 +1,9 @@
+package pe.edu.tecsup.lab03.controllers
+
 @Override
 public String toString() {
     return "Student{id=" + id + ", nombre='" + nombre + "', apellido='" + apellido + "'}";
+}
+
+void main() {
 }
